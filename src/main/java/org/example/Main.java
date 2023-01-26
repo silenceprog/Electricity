b6d868121340ec5659cdbade821dc5f86338afdb
Max Komar <silencecut@gmail.com>
2023-01-26 14:43:55 +0200

    Улучшение кода, добавление json ответа

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,32 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        Schedules schedule = new Schedules();
+
         UnaryOperator<String> parser = url -> {
             try {
                 Document document = Jsoup.connect(url)
                         .get();
-                return document.body().text();
+                Element body = document.body();
+                schedule.setDate(body.select("span").text());
+                schedule.setInfo(body.select("h5").text());
+                schedule.setfSchedule(body.select("label.text-danger").text());
+                schedule.setsSchedule(body.select("label.text-warning").text());
+                return gson.toJson(schedule);
+
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -20,3 +34,8 @@ public class Main {
 
     }
 }
+
+
+
+
+
---
+++ src/main/java/org/example/Schedules.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Schedules {
+    private String date;
+    private String info;
+    private String fSchedule;
+    private String sSchedule;
+
+    Schedules(){
+
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public void setfSchedule(String fSchedule) {
+        this.fSchedule = fSchedule;
+    }
+
+    public void setsSchedule(String sSchedule) {
+        this.sSchedule = sSchedule;
+    }
+
+
+}
